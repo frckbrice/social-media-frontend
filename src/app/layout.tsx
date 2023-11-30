d33e5ae9dcdfa84ba9a7be7a577f5d4d45bb6345
gmarvis <sgmarvis@gmail.com>
2023-11-30 10:00:04 +0100

    added the reuseable avatar component and the discussions layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="">{children}</body>
-    </html>
+    <body className="flex flex-col h-screen absolute w-[100vw] items-center">
+      <div className="h-[32vh] w-full bg-themecolor absolute top-0"></div>
+      <div className=" flex flex-col items-center w-full mx-auto relative ">
+        {children}
+      </div>
+    </body>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,36 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        greenMessage: "#8EFD8E",
+        themecolor: "#01a884",
+        secondry: "#005c4b",
+        bgGray: "#f0f2f5",
+
+        btn: {
+          background: "hsl(var(--btn-background))",
+          "background-hover": "hsl(var(--btn-background-hover))",
+        },
+      },
+      screens: {
+        mobile: "0px",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
