bd14486848759a4437ca87e82a21e4bf29720cdb
ricardo <ricardotsopse@gmail.com>
2023-12-01 18:44:01 +0100

    signup onboarding flow

--- src/components/atoms/googleButton.tsx
+++ src/components/atoms/googleButton.tsx
@@ -1,42 +1,17 @@
 "use client";
 import Image from "next/image";
-import React, { useState } from "react";
+import React from "react";
 
 const GoogleButton = () => {
-  const [isLoading, setIsLoading] = useState<Boolean>(false);
-
-  const handleGoogleSignin = async () => {
-    const { data, error } = await supabase.auth.signInWithOAuth({
-      provider: "google",
-      options: {
-        queryParams: {
-          access_type: "offline",
-          prompt: "consent",
-        },
-        redirectTo: urlToUse(),
-      },
-    });
-    setIsLoading(true);
-  };
-
   return (
     <div>
-      <button
-        onClick={() => handleGoogleSignin()}
-        data-u
-        className={
-          isLoading
-            ? "hover:cursor-not-allowed"
-            : "flex border border-themecolor text-xl items-center rounded p-4 gap-3 px-8   font-bold m-auto"
-        }
-      >
+      <button className="flex border border-themecolor text-xl items-center rounded p-4 gap-3 px-8   font-bold m-auto">
         <Image
           height={40}
           width={40}
           alt="google logo"
           src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png"
         />
-        {/* <p>{isLoading ? <Pulsation /> : "Sign In with Google"}</p> */}
       </button>
     </div>
   );
--- src/components/organisms/signupb.tsx
+++ src/components/organisms/signupb.tsx
@@ -1,43 +1,8 @@
 "use client";
-import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React from "react";
 import PulseLoader from "../atoms/pulseLoader";
 
 const Signupb = () => {
-  const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleInputChange = async () => {
-    setIsLoading(true);
-    const googleUser = JSON.parse(
-      localStorage.getItem("sb-xkwspfurbsmpwwazlkmu-auth-token") || "{}"
-    );
-    const { data } = await supabase.from("user").select("email");
-    let res = data?.filter((i) => i.email === googleUser.user.email);
-    if (res?.length === 1) {
-      LOCAL_STORAGE.save("email", googleUser.user.email);
-      setSuccess("Welcome back 🙂");
-      router.push("/discussions");
-      setIsLoading(false);
-      return;
-    }
-    if (res?.length === 0) {
-      LOCAL_STORAGE.save("email", googleUser.user.email);
-      const { data, error } = await supabase.from("user").insert({
-        email: googleUser.user.email,
-        name: googleUser.user.user_metadata.name,
-        image: googleUser.user.user_metadata.picture,
-        phone: googleUser.user.identities.phone,
-      });
-      if (error) console.log("an error occured while sending user", error);
-      console.log("data from DB", data);
-      router.push("/discussions");
-      setIsLoading(false);
-    }
-  };
-
   return (
     <div className=" mt-56 items-center justify-center text-center">
       <h1 className="text-3xl font-extrabold text-themecolor font-serif">
@@ -48,13 +13,10 @@ const Signupb = () => {
         'Agree and Continue' to accept the{" "}
         <span className="text-themecolor">Terms of Service</span>
       </h4>
-      <button
-        onClick={() => handleInputChange()}
-        className="border p-4 px-5 text-base font-extrabold text-black rounded"
-      >
-        {isLoading ? <PulseLoader /> : "Agree and Continue"}
+      <button className="border p-4 px-5 text-base font-extrabold text-black rounded">
+        {<PulseLoader />}
       </button>
-      <p className="text-2xl mt-6 font-extrabold text-themecolor">{success}</p>
+      <p className="text-2xl mt-6 font-extrabold text-themecolor">success</p>
     </div>
   );
 };
