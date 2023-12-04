2241b4fd99bf0f9c8a8d97a8cc201afca81822fb
nsamedaisy <nsamedaisy@gmail.com>
2023-12-04 16:01:33 +0100

    working on a user contact information

--- src/app/discussions/[id]/page.tsx
+++ src/app/discussions/[id]/page.tsx
@@ -33,76 +33,71 @@ const Chats = () => {
   };
 
   return (
-
     <div className="w-full ">
-
-      
-
-
-    <div className="relative flex flex-col h-full">
-
-        <div className="flex items-center justify-between p-2 bg-bgGray border-l-2 w-full ">
+      <div className="relative flex flex-col h-full">
+        <div className="flex items-center justify-between p-2 h-16 bg-bgGray border-l-2 w-full ">
           <div className="flex items-center">
             <Avatar
               size={4}
               profilePicture="https://i.pinimg.com/564x/17/f7/ba/17f7baaff77ee55d8807fcd7b2d2f47a.jpg"
               onClick={handleAvatarClick}
             />
-            <h1 className="ml-4 text-lg">John Doe</h1>
+            <div className="ml-4 ">
+              <h1 className="text-lg">John Doe</h1>
+              <span className="text-gray-500">online/offline</span>
+            </div>
           </div>
           <div className="flex items-center text-gray-500 text-xl">
             <FaSearch className="mr-8" />
             <FaEllipsisV className="mr-2" />
           </div>
-      </div>
+        </div>
 
-      <div className="w-full relative flex-grow overflow-auto h-[80vh]">
-        <div
-          className="bg-fixed  w-[71.2%] h-[80%] fixed z-0"
-          style={{
-            backgroundImage:
-              "url('https://i.pinimg.com/600x315/8c/98/99/8c98994518b575bfd8c949e91d20548b.jpg')",
-          }}
-        >
-          <p className="rounded-md shadow-md text-gray-500 w-20 px-1 py-2 bg-white text-center text-lg ml-[45%]">
-            Today
-          </p>
-          <p className="text-myG w-[48vw] ml-[15%] font-semibold p-2 rounded-md mt-5 flex text-sm text-center bg-yellow justify-center">
-            <FaLock className="mr-2" /> Messages are end-to-end encrypted. No
-            one outside of this chat, not even WaxChat, can read or listen to
-            them.
-          </p>
-          {/* Render chat messages here */}
+        <div className="w-full relative flex-grow overflow-auto h-[80vh]">
+          <div
+            className="bg-fixed  w-[71.2%] h-[80%] fixed z-0"
+            style={{
+              backgroundImage:
+                "url('https://i.pinimg.com/600x315/8c/98/99/8c98994518b575bfd8c949e91d20548b.jpg')",
+            }}
+          >
+            <p className="rounded-md shadow-md text-gray-500 w-20 px-1 py-2 bg-white text-center text-lg ml-[45%]">
+              Today
+            </p>
+            <p className="text-myG w-[48vw] ml-[15%] font-semibold p-2 rounded-md mt-5 flex text-sm text-center bg-yellow justify-center">
+              <FaLock className="mr-2" /> Messages are end-to-end encrypted. No
+              one outside of this chat, not even WaxChat, can read or listen to
+              them.
+            </p>
+            {/* Render chat messages here */}
+
+            <p>Sender</p>
+            <p>Reciever</p>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-between p-4 h-20 text-2xl text-gray-500  bg-purple-500">
+          <AiOutlineSmile className="mr-5 text-myG text-4xl" />
+          <FaPlus className="mr-2 text-gray-500" />
+          <input
+            type="text"
+            placeholder="Type a message"
+            value={message}
+            onChange={handleChange}
+            className="w-full p-3 bg-white border-0 rounded-md focus:outline-none mx-6 text-lg"
+          />
+          {message.length === 0 ? (
+            <FaMicrophone className="text-gray-600" />
+          ) : (
+            <FaPaperPlane
+              className="mr-2 text-gray-500 cursor-pointer"
+              onClick={handleSendMessage}
+            />
+          )}
         </div>
       </div>
 
-      <div className="flex items-center justify-between p-4 text-2xl text-gray-500  bg-purple-500">
-        <AiOutlineSmile className="mr-5 text-myG text-4xl" />
-        <FaPlus className="mr-2 text-gray-500" />
-        <input
-          type="text"
-          placeholder="Type a message"
-          value={message}
-          onChange={handleChange}
-          className="w-full p-3 bg-white border-0 rounded-md focus:outline-none mx-6 text-lg"
-        />
-        {message.length === 0 ? (
-          <FaMicrophone className="text-gray-600" />
-        ) : (
-          <FaPaperPlane
-            className="mr-2 text-gray-500 cursor-pointer"
-            onClick={handleSendMessage}
-          />
-        )}
-      </div>
-    </div>
-
-
-
-    <div>
-      {/* <InfoCard /> */}
-    </div>
-
+      <div>{/* <InfoCard /> */}</div>
     </div>
   );
 };
