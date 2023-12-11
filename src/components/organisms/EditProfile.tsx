0d54806ae445ddfc49c4eb279c5fcd99110e4068
Gael Tikeng <Gealinho@yahoo.fr>
2023-12-11 10:56:07 +0100

    update function

--- src/components/organisms/EditProfile.tsx
+++ src/components/organisms/EditProfile.tsx
@@ -13,13 +13,14 @@ import { RiPencilFill } from "react-icons/ri";
 import { updateProfileName, uplaodImage } from "@/utils/service/queries";
 import { IoMdClose } from "react-icons/io";
 import { SITE_URL } from "@/utils/service/constant";
+import { toast } from "react-toastify";
+import { TbHourglassEmpty } from "react-icons/tb";
 
 const EditProfile = () => {
   const [onEditName, setOnEditName] = useState(false);
   const [onEditAbout, setOnEditAbout] = useState(false);
   const { currentUser } = useAppContext();
   const [userName, setUserName] = useState(currentUser?.name);
-  const [about, setAbout] = useState("")
 
   const [profilePhoto, setProfilePhoto] = useState(currentUser?.image);
 
@@ -27,22 +28,22 @@ const EditProfile = () => {
 
   const email = JSON.parse(localStorage.getItem('email') || '')
   const handleUpdateName = async () => {
-    try {
-      const payload = {name: userName}
-      const response = await fetch(SITE_URL + `/rooms/${currentUser.id}`, {
-        method: 'PUT',
-        body: JSON.stringify(payload),
-      })
-      if (!response.ok) {
-        throw new Error('unable to update profile name')
-      }
-      const data = response.json()
-      console.log('this is new user OBJECT', data)
+    const update = { name: userName }
 
-    } catch (error) {
-      console.error(error)
-    }
-    setOnEditName((prev) => !prev);
+    await updateProfileName(currentUser.id, update)
+      .then((res) => {
+        localStorage.setItem('sender', JSON.stringify(res))
+        setOnEditName((prev) => !prev);
+        toast.success("UserName updated", {
+          position: "top-right",
+          hideProgressBar: true,
+          autoClose: 2000,
+        })
+        console.log(res)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   };
 
   const handleUpdateAbout = () => {
